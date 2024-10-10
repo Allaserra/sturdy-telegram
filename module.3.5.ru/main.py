b06c9d52e79b364723cf1abb261741c5c04685f6
Allaserra <544698xa@mail.ru>
2024-10-10 08:11:47 +0300

    Add files via upload

---
+++ module.3.5.ru/main.py
@@ -0,0 +1,17 @@
+def get_multiplied_digits (number):
+    str_number = str(number)
+    first = int(str_number[0])
+    sec = 1
+    if len(str_number) > 1:
+        sec *= first
+    else:
+        return sec * first
+    return first * get_multiplied_digits(int(str_number[1:]))
+
+
+print(get_multiplied_digits(40203))
+print(get_multiplied_digits(506076))
+print(get_multiplied_digits(321))
+print(get_multiplied_digits(25003))
+
+
